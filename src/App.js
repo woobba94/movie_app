74592ce714ae8e12a7adaa97f83bb6577898f731
woobba94 <jwj3199@naver.com>
2020-07-21 01:32:27 +0900

    p.97

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+function Food({name, picture}){
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src = {picture} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png',
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://img.danawa.com/prod_img/500000/100/698/img/8698100_1.jpg?shrink=500:500&_v=20190717160831',
+  },
+];
+
+function renderFood(dish){
+  return <Food name = {dish.name} picture = {dish.image}/>;
+}
+
 function App() {
-  return <div className="App"/>;
+  return (
+    <div>
+      {foodILike.map(renderFood)}
+    </div>
+  );
 }
 
 export default App;
