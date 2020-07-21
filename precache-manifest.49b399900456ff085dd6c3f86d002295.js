d5afd6ef5a8dcf01cd01ad64f2b8dcdd7ce9cae3
woobba94 <jwj3199@naver.com>
2020-07-22 01:54:50 +0900

    Updates

---
+++ precache-manifest.49b399900456ff085dd6c3f86d002295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce595e5bb06397823ce7e6ef985ca8f0",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "50473160577731a5888c",
+    "url": "/movie_app/static/css/main.2c94eb48.chunk.css"
+  },
+  {
+    "revision": "e6ee8035cbef69ff6a6d",
+    "url": "/movie_app/static/js/2.ec260248.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.ec260248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50473160577731a5888c",
+    "url": "/movie_app/static/js/main.0b53b366.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
